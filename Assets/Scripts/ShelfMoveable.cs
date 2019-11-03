b0ff9aa8686b32955e7291162573b621fd4738fa
Maksymilian Łakomy <zommer128@gmail.com>
2019-11-03 01:58:59 +0100

    Minor changes

--- Assets/Scripts/ShelfMoveable.cs
+++ Assets/Scripts/ShelfMoveable.cs
@@ -8,6 +8,7 @@ public class ShelfMoveable : MonoBehaviour {
     private Vector3 startPosition;
     [SerializeField] private Transform endPosition;
     private bool toEndPosition = true;
+    [SerializeField] [Range(1f, 6f)] float moveSpeed = 1f;
 
     private void Awake() {
         startPosition = transform.position;
@@ -16,12 +17,12 @@ public class ShelfMoveable : MonoBehaviour {
     private void Update() {
         if (toEndPosition == true) {
             transform.position =
-                Vector3.MoveTowards(transform.position, new Vector3(endPosition.position.x, transform.position.y, transform.position.z), Time.deltaTime /1f);
+                Vector3.MoveTowards(transform.position, new Vector3(endPosition.position.x, transform.position.y, transform.position.z), Time.deltaTime * moveSpeed);
             if (Mathf.Round(transform.position.x) == Mathf.Round(endPosition.position.x))
                 toEndPosition = false;
         } else {
             transform.position =
-                Vector3.MoveTowards(transform.position, new Vector3(startPosition.x, transform.position.y, transform.position.z), Time.deltaTime /1f);
+                Vector3.MoveTowards(transform.position, new Vector3(startPosition.x, transform.position.y, transform.position.z), Time.deltaTime * moveSpeed);
             if (Mathf.Round(transform.position.x) == Mathf.Round(startPosition.x))
                 toEndPosition = true;
         }
