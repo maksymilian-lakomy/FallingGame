040886c3dde30149f4856e2c53d27b0faf69c943
Maksymilian Łakomy <zommer128@gmail.com>
2019-11-03 01:36:58 +0100

    Added moveable shelf

---
+++ Assets/Scripts/ShelfMoveable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfMoveable : MonoBehaviour {
+    
+    private Vector3 startPosition;
+    [SerializeField] private Transform endPosition;
+    private bool toEndPosition = true;
+
+    private void Awake() {
+        startPosition = transform.position;
+    }
+
+    private void Update() {
+        if (toEndPosition == true) {
+            transform.position =
+                Vector3.MoveTowards(transform.position, new Vector3(endPosition.position.x, transform.position.y, transform.position.z), Time.deltaTime /1f);
+            if (Mathf.Round(transform.position.x) == Mathf.Round(endPosition.position.x))
+                toEndPosition = false;
+        } else {
+            transform.position =
+                Vector3.MoveTowards(transform.position, new Vector3(startPosition.x, transform.position.y, transform.position.z), Time.deltaTime /1f);
+            if (Mathf.Round(transform.position.x) == Mathf.Round(startPosition.x))
+                toEndPosition = true;
+        }
+    }
+}
