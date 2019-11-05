de1ccc86fba3ab1b231a1ae3c8dc097bffcf8ea3
Maksymilian Łakomy <zommer128@gmail.com>
2019-11-05 23:18:03 +0100

    Improved bar handling

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -17,15 +16,17 @@ public class UIManager : MonoBehaviour {
     [SerializeField] private List<Scene> levels = new List<Scene>();
 
     [SerializeField] private Rigidbody playerRB;
+
+    [SerializeField] private Color panelColor = new Color(0.8901961f, 0.7294118f, 0.4f, 1f);
+    [SerializeField] private Color dangerColor = Color.red;
     
     void Update() {
-        if (!playerRB)
-            return;
-        velocityBar.fillAmount = Mathf.Lerp(velocityBar.fillAmount, Mathf.Abs(playerRB.velocity.y / 10f), Time.deltaTime*10f);
-        if (playerRB.velocity.y <= -7f)
-            velocityBar.color = Color.red;
-        else
-            velocityBar.color = new Color(0.8901961f, 0.7294118f, 0.4f, 1f);
+        if (Player.i.PlayerState == PlayerState.Alive)
+            velocityBar.fillAmount = Mathf.Lerp(velocityBar.fillAmount, Mathf.Abs(playerRB.velocity.y / 10f), Time.deltaTime*10f);
+            if (playerRB.velocity.y <= -6f)
+                velocityBar.color = Color.Lerp(velocityBar.color, dangerColor, Time.deltaTime * 2f);
+            else
+                velocityBar.color = Color.Lerp(velocityBar.color, panelColor, Time.deltaTime * 2f);
 
 
     }
