1c913e4820cd98ceed4c51a0293789d8729fd285
Maksymilian Łakomy <zommer128@gmail.com>
2019-11-05 17:05:10 +0100

    Added hangers

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    [SerializeField] public Transform ObjectToFollow;
+
+    private void LateUpdate() {
+        transform.position = ObjectToFollow.position;
+    }
+}
