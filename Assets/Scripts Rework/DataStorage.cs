696936101051aeb2dbe038fe409d5063d6d4a44d
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-06 20:35:43 +0100

    Update DataStorage.cs
    Changed getters

--- Assets/Scripts Rework/DataStorage.cs
+++ Assets/Scripts Rework/DataStorage.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class DataStorage : MonoBehaviour, ICollectListenable {
 
-    public int CurrentCupIndex = 0;
-    public List<CupStorage> UnlockedCups = new List<CupStorage>();
-    public int SugarCubeAmount = 0;
+    public static DataStorage Instance;
 
+    public int CurrentCupIndex { get; private set; } = 0;
+    public List<CupStorage> UnlockedCups { get; private set; } = new List<CupStorage>();
+    public int SugarCubeAmount { get; private set; } = 0;
+
+    private void Awake() {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(this);
+    }
+    
     public void ChangeSugarCubesAmount(int amount) {
         throw new NotImplementedException();
     }
