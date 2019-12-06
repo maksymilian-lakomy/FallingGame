63efddbd71388f63dd396d5da0b81a4eccc67e0c
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-06 19:52:08 +0100

    Refactoring all systems #5 - Implemented input manager
    Not tested

---
+++ Assets/Scripts Rework/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public static class InputManager {
+    public static Vector2 GetHorizontalInput() {
+        Vector2 horizontalInput = GetKeyboardHorizontalInput();
+        if (horizontalInput == Vector2.zero)
+            horizontalInput = GetTouchHorizontalInput();
+        return horizontalInput;
+    }
+    
+    private static Vector2 GetKeyboardHorizontalInput() {    
+        return new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+    }
+    
+    private static Vector2 GetTouchHorizontalInput() {
+        if (Input.touchCount <= 0 || Input.GetTouch(0).phase != TouchPhase.Began)
+            return Vector2.zero;
+        float screenCenterAxis = Screen.width / 2f;
+        Touch touch = Input.GetTouch(0);
+        if (touch.position.x >= screenCenterAxis)
+            return Vector2.right;
+        else
+            return Vector2.left;
+    }
+}
