c1cca6f1cb65567a75cdb72397b32a307a8231f6
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-06 19:21:14 +0100

    Refactoring all systems #4 - Collectable objects

---
+++ Assets/Scripts Rework/CollectableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class CollectableObject : MonoBehaviour {
+    
+    [SerializeField]
+    protected List<ICollectListenable> collectListeners = new List<ICollectListenable>();
+    private void OnTriggerEnter(Collider other) {
+        Collect();
+    }
+
+    protected abstract void Collect();
+}
---
+++ Assets/Scripts Rework/SugarCubeObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SugarCubeObject : CollectableObject {
+
+    [SerializeField]
+    private int changeAmount = 0;
+    
+    protected override void Collect() {
+        throw new System.NotImplementedException();
+    }
+}
