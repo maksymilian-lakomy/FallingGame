a8a3874e188bdbf8172f2e4ac9338a66e70ea072
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-06 19:06:15 +0100

    Refactoring all systems #2 - GameManager

---
+++ Assets/Scripts Rework/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour, ISmashListenable {
+    private CupController cupController;
+    private Transform levelStart;
+
+    public void InitializeGame() {
+        throw new NotImplementedException();
+    }
+    
+    public void RestartGame() {
+        throw new NotImplementedException();
+    }
+
+    public void OnSmash() {
+        throw new NotImplementedException();
+    }
+}
