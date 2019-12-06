8f23a2675c9493b3ccec1d80b6aaa2f7e540bd7d
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-06 19:03:03 +0100

    Refactoring all systems #1

---
+++ Assets/Scripts Rework/CupController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupController : MonoBehaviour {
+    private int jumpCounter = 0;
+    [SerializeField]
+    private List<ISmashListenable> smashListeners = new List<ISmashListenable>();
+
+    private void Awake() {
+        throw new NotImplementedException();
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        throw new NotImplementedException();
+    }
+
+    private void Movement() {
+        throw new NotImplementedException();
+    }
+
+    private bool CanJump() {
+        throw new NotImplementedException();
+    }
+
+    private void ResetJumpCounter() {
+        throw new NotImplementedException();
+    }
+
+    private void OnFloorCollision() {
+        throw new NotImplementedException();
+    }
+
+    private bool IsOverspeed() {
+        throw new NotImplementedException();
+    }
+
+    private void Smash() {
+        throw new NotImplementedException();
+    }
+    
+    
+}
