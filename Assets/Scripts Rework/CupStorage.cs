004e6aa5910852d23b1968c29a03c5ba0f9047b7
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-06 19:15:27 +0100

    Refactoring all systems #3 - DataStorage

---
+++ Assets/Scripts Rework/CupStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CupStorage {
+    public int Key;
+    public GameObject Prefab;
+}
---
+++ Assets/Scripts Rework/DataStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStorage : MonoBehaviour, ICollectListenable {
+
+    public int CurrentCupIndex = 0;
+    public List<CupStorage> UnlockedCups = new List<CupStorage>();
+    public int SugarCubeAmount = 0;
+
+    public void ChangeSugarCubesAmount(int amount) {
+        throw new NotImplementedException();
+    }
+    
+    public void OnCollect(int amount) {
+        ChangeSugarCubesAmount(amount);
+    }
+}
