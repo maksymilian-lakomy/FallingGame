b9bf4d279bab983e0fb8943cfe73d43d4e8b8bff
Maksymilian Łakomy <zommer128@gmail.com>
2019-12-07 02:37:20 +0100

    UI handler rework
    Changed everything to interface events

--- Assets/Scripts Rework/GameManager.cs
+++ Assets/Scripts Rework/GameManager.cs
@@ -3,35 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour, ISmashListenable {
+
+    public static GameManager Instance;
+    
     [SerializeField]
     private CupController cupController;
     [SerializeField]
     private Transform levelStart;
 
+    private GameState gameState;
+    
+    public GameState GameState {
+        get => gameState;
+        private set {
+            foreach (IGameStateListenable listener in gameStateListeners) {
+                listener.OnGameStateChange(value);
+            }
+            gameState = value;
+        }
+    }
+
     private IEnumerable<ICupCreateListenable> cupCreateListeners;
+    private IEnumerable<IGameStateListenable> gameStateListeners;
+    
 
     private void Awake() {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
         cupCreateListeners = FindObjectsOfType<MonoBehaviour>().OfType<ICupCreateListenable>();
+        gameStateListeners = FindObjectsOfType<MonoBehaviour>().OfType<IGameStateListenable>();
         InitializeGame();
     }
     
     private void InitializeGame() {
         GameObject cup = Instantiate(DataStorage.Instance.GetCup(), levelStart.position, levelStart.rotation);
         cupController = cup.GetComponent<CupController>();
+        GameState = GameState.Active;
         foreach (ICupCreateListenable listener in cupCreateListeners) {
             listener.OnCupCreate(cup);
         }
     }
     
     public void RestartGame() {
-        throw new NotImplementedException();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void OnSmash(GameObject sender) {
         Transform position = sender.transform;
         Destroy(sender);
         Instantiate(DataStorage.Instance.GetSmashedCup(), position.position, position.rotation);
+        GameState = GameState.Ended;
     }
 }
---
+++ Assets/Scripts Rework/UIAnimatorHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UIAnimatorHandler : MonoBehaviour, IGameStateListenable {
+    private Animator animator;
+    
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void DisplayUI(GameState gameState) {
+        switch (gameState) {
+            case GameState.Ended: 
+                animator.SetBool("Ended", true);
+                break;
+            case GameState.Active:
+                animator.SetBool("Ended", false);
+                break;
+        }
+    }
+    
+    public void OnGameStateChange(GameState gameState) {
+        DisplayUI(gameState);
+    }
+}
